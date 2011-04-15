19103397e63a5e45b194bf34b96398c3ca0381bb
teamkde@gmail.com <teamkde@gmail.com@9c272762-e0f0-3911-e4ab-75af7bd3cf6c>
2011-04-15 13:32:40 +0000

    Initial move of code to Google Code Hosting.

---
+++ trunk/ce-mobile-crm/js/LanguageResources.js
@@ -0,0 +1,88 @@
+/* General Resources */
+var RES_LOGIN_MESSAGE = "Enter your username and password, then click login";
+var RES_LOGIN_TITLE = "Login";
+var RES_LOGOUT_LABEL = "Logout"
+var RES_HOME_LABEL = "Home";
+var RES_BACK_LABEL = "Back Test";
+var RES_USERNAME_LABEL = "Username";
+var RES_PASSWORD_LABEL = "Password";
+var RES_ACCOUNTS_LABEL = "Accounts";
+var RES_ACCOUNT_LABEL = "Account";
+var RES_CONTACTS_LABEL = "Contacts";
+var RES_CONTACT_LABEL = "Contact";
+var RES_OPPORTUNITIES_LABEL = "Opportunities";
+var RES_OPPORTUNITY_LABEL = "Opportunity";
+var RES_LEADS_LABEL = "Leads";
+var RES_LEAD_LABEL = "Lead";
+var RES_CALLS_LABEL = "Calls";
+var RES_CALL_LABEL = "Call";
+var RES_MEETINGS_LABEL = "Meetings";
+var RES_MEETING_LABEL = "Meeting";
+var RES_TASKS_LABEL = "Tasks";
+var RES_TASK_LABEL = "Task";
+var RES_NOTES_LABEL = "Notes";
+var RES_NOTE_LABEL = "Note";
+var RES_CAMPAIGN_LABEL = "Campaign";
+var RES_CAMPAIGNS_LABEL = "Campaigns";
+var RES_USER_LABEL = "User";
+var RES_USERS_LABEL = "Users";
+/* Divider Resources */
+var RES_OVERVIEW_LABEL = "Overview";
+var RES_MORE_INFORMATION_LABEL = "More Information";
+var RES_OTHER_LABEL = "Other";
+/* Account Field Label Resources */
+var RES_ACCOUNT_BILLING_ADDRESS_LABEL = "Billing Address";
+var RES_ACCOUNT_SHIPPING_ADDRESS_LABEL = "Shipping Address";
+var RES_ACCOUNT_INDUSTRY_LABEL = "Industry";
+var RES_ACCOUNT_REVENUE_LABEL = "Annual Revenue";
+var RES_ACCOUNT_EMPLOYEES_LABEL = "Employees";
+var RES_ACCOUNT_SIC_LABEL = "SIC Code";
+var RES_ACCOUNT_TICKER_LABEL = "Ticker Symbol";
+var RES_ACCOUNT_MEMBER_LABEL = "Member Of";
+var RES_ACCOUNT_OWNERSHIP_LABEL = "Ownership";
+var RES_ACCOUNT_CAMPAIGN_LABEL = "Campaign";
+var RES_ACCOUNT_RATING_LABEL = "Rating";
+/* Contact Field Label Resources */
+var RES_CONTACT_REPORTS_TO_LABEL = "Reports To";
+/* Opportunity Field Label Resources */
+var RES_OPPORTUNITY_NAME_LABEL = "Opportunity Name";
+var RES_OPPORTUNITY_AMOUNT_WITH_CURRENCY_LABEL = "Opportunity Amount ({0} {1})";
+var RES_OPPORTUNITY_AMOUNT_LABEL = "Opportunity Amount";
+var RES_OPPORTUNITY_EXPECTED_CLOSE_DATE_LABEL = "Epected Close Date";
+var RES_OPPORTUNITY_SALES_STAGE_LABEL = "Sales Stage";
+var RES_OPPORTUNITY_PROBABILITY_LABEL = "Probability";
+var RES_OPPORTUNITY_NEXT_STEP_LABEL = "Next Step";
+/* Lead Field Label Resources */
+var RES_LEAD_REFERRED_BY = "Referred By";
+/* Task Field Label Resources */
+var RES_TASK_DUE_DATE_LABEL = "Due Date";
+var RES_TASK_PRIORITY_LABEL = "Priority";
+/* General Field Label Resources */
+var RES_OFFICE_PHONE_LABEL = "Office Phone";
+var RES_FAX_LABEL = "Fax";
+var RES_DESCRIPTION_LABEL = "Description";
+var RES_EMAIL_ADDRESS_LABEL = "Email Address";
+var RES_LEAD_SOURCE_LABEL = "Lead Source";
+var RES_RELATED_ITEM_NAME_LABEL = "Name";
+var RES_TYPE_LABEL = "Type";
+var RES_NAME_LABEL = "Name";
+var RES_TITLE_LABEL = "Title";
+var RES_MOBILE_PHONE_LABEL = "Mobile";
+var RES_DEPARTMENT_LABEL = "Department";
+var RES_PRIMARY_ADDRESS_LABEL = "Primary Address";
+var RES_OTHER_ADDRESS_LABEL = "Other Address";
+var RES_WEBSITE_LABEL = "Website";
+var RES_SUBJECT = "Subject";
+var RES_STATUS_LABEL = "Status";
+var RES_START_DATE_TIME_LABEL = "Start Date & Time";
+var RES_START_DATE_LABEL = "Start Date";
+var RES_DURATION_LABEL = "Duration";
+var RES_LOCATION_LABEL = "Location";
+var RES_REMINDER_LABEL = "Reminder";
+var RES_DETAILS_LABEL = "Details";
+var RES_ASSIGNED_TO_LABEL = "Assigned To";
+var RES_DATE_MODIFIED_LABEL = "Date Modified";
+var RES_DATE_CREATED_LABEL = "Date Created";
+/* Notification Resources */
+var RES_DO_NOT_CALL = "*NOTE: This Contact is marked as Do Not Call.";
+var RES_LOG_CALL_QUESTION = "Would you like to log this call?";
