e4c62c82063bad65073bd60d485ea0cfdef9f7b5
jmdelehaye@gmail.com <jmdelehaye@gmail.com@9c272762-e0f0-3911-e4ab-75af7bd3cf6c>
2011-06-18 19:49:59 +0000

    created branch

---
+++ branches/jpisard/ce-mobile-crm/js/language/fr_FR.js
@@ -0,0 +1,16 @@
+var Hometitle='Acceuil';
+
+	var HomePageiconlabel = {
+	  "Accounts" : "Comptes",
+	  "Contacts" : "Contacts",
+	  "Opportunities" : "Opportunities",
+	  "Leads" : "Leads",
+	  "Calls" : "Appels",
+	  "Meetings" : "Reunions",
+	  "Tasks" : "Taches"
+	};
+var Accountstitle='Comptes';
+var AccountDetailstitle='Detail du Compte';
+var Mobilebacktext='Retour';
+
+
