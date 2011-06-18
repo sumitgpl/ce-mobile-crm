2344ebc4fe1a690d65e37e9c1b2d7257747f267a
jmdelehaye@gmail.com <jmdelehaye@gmail.com@9c272762-e0f0-3911-e4ab-75af7bd3cf6c>
2011-06-18 22:48:10 +0000

    added notes list

---
+++ branches/notes/ce-mobile-crm/js/select_field.js
@@ -0,0 +1,20 @@
+
+var AccountsListField='["name","billing_address_city","billing_address_state"]';
+var AccountDetailsField='["name","description","phone_office","phone_fax","email1","website","billing_address_city","billing_address_street","billing_address_state","billing_address_postalcode","billing_address_country","shipping_address_street","shipping_address_state","shipping_address_postalcode","shipping_address_city","shipping_address_country","account_type","industry","employees","sic_code","ticker_symbol","parent_name","ownership","campaign_name","rating","assigned_user_name","annual_revenue"]';
+var ContactListField='["first_name","last_name","title","account_name"]';
+var ContactDetailsField='["first_name","last_name","account_name","title","phone_work","phone_mobile","email1","description","primary_address_city","primary_address_street","primary_address_state","primary_address_postalcode","primary_address_country"]';
+var CallsListField='["name","date_start","direction","status"]';
+var CallsDetailsField='["name","direction","status","date_start","duration_hours","duration_minutes","description","assigned_user_name","date_modified","modified_by_name","date_entered","created_by_name","parent_id"]';
+
+var OpportunitiesListField='["name","amount","currency_name","currency_symbol","amount_usdollar","sales_stage"]';
+var OpportunitiesDetailsField='["name","amount","currency_name","currency_symbol","amount_usdollar","sales_stage","date_closed","opportunity_type","probability","lead_source","next_step","description","assigned_user_name","date_modified","modified_by_name","date_entered","created_by_name"]';
+
+var LeadsListField='["first_name","last_name","title","account_name"]';
+var LeadsDetailsField='["first_name","last_name","account_name","title","phone_work","phone_mobile","email1","description","primary_address_city","primary_address_street","primary_address_state","primary_address_postalcode","primary_address_country","status","lead_source","status_description","lead_source_description","opportunity_amount","refered_by","campaign_name","assigned_user_name","date_modified","modified_by_name","date_entered","created_by_name","do_not_call"]';
+
+var MeetingsListField='["name","date_start","status"]';
+var MeetingsDetailsField='["name","date_start","status","duration_hours","duration_minutes","description","assigned_user_name","date_modified","modified_by_name","date_entered","created_by_name","parent_id"]';
+
+var TasksListField='["name","status","date_due"]';
+var TasksDetailsField='["name","status","date_due","assigned_user_name","date_modified","modified_by_name","date_entered","created_by_name","parent_id"]';
+
